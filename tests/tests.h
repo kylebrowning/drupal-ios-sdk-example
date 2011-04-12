55cfdfdc3c8e2745a5e00be52bd6e1bc768563ba
Kyle Browning <kylebrowning@me.com>
2011-04-11 17:07:41 -0700

    Initial commit of testing env

---
+++ tests/tests.h
@@ -0,0 +1,17 @@
+//
+//  tests.h
+//  tests
+//
+//  Created by Kyle Browning on 4/11/11.
+//  Copyright 2011 Grasscove. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface tests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ tests/tests.m
@@ -0,0 +1,33 @@
+//
+//  tests.m
+//  tests
+//
+//  Created by Kyle Browning on 4/11/11.
+//  Copyright 2011 Grasscove. All rights reserved.
+//
+
+#import "tests.h"
+
+
+@implementation tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in tests");
+}
+
+@end
